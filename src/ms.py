bb62cf4b5a75f478dd35c385d98477d62bb1e05f
MJ <mv1003@nyu.edu>
2015-03-26 18:21:53 -0400

    Update ms.py

--- src/ms.py
+++ src/ms.py
@@ -21,6 +21,14 @@ def imext(y):
   
   return yy
 
+
+def ext(y):
+   n = int(np.sqrt(y.shape[0]))
+   y = y.reshape(n,n)
+   yy = np.zeros((n+2, n+2))
+   yy[1:-1,1:-1] = y
+   return yy.reshape(n+2,n+2)  
+
 def B(n):
 
   """ n+1 = number of rows 
@@ -33,7 +41,7 @@ def B(n):
   for i in range(1,n+1):
     b[i , i-1:i-1+3] = np.array([1,4,1])
 
-  return b
+  return b[1:-1,1:-1]
 
 def phi(x, n):
 
@@ -50,4 +58,4 @@ def phi(x, n):
     #print i
     #print matrix[i, i-2:i-2+5]
     matrix[i, i-2:i-2+5] = np.array([p1,p2,p3,p4,p5])
-  return matrix
+  return matrix[1:-1,1:-1]
