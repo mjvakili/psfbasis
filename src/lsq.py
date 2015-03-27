f0f235f5ca0a115f67cc86a9f5daf0db908a7786
MJ <mv1003@nyu.edu>
2015-03-27 12:54:25 -0400

    Update lsq.py

--- src/lsq.py
+++ src/lsq.py
@@ -1,36 +1,43 @@
 import numpy as np
 import ms
-import modelshifter
+import shifter
 import shift
 import numpy as np
 import scipy.optimize as op
-from scipy.linalg import solve
-
+from   scipy.linalg import solve
+import scipy.linalg as la
 
 class stuff(object):
    
-    def __init__(self, data, K, min_iter=5, max_iter=30, check_iter=5 , tol=1.e-6):
+    def __init__(self, data, Q, min_iter=5, max_iter=30, check_iter=5 , tol=1.e-6):
 
-        #for i in range(data):
-          
-          
+        """ inputs of the code: NxD data matrix;
+                                NxD uncertainty matrix;
+                                Q = latent dimensionality
+        """
 
-        self.N = data.shape[0]
-        self.D = data.shape[1]
-        self.K = K 
-        self.data = np.atleast_2d(data)
-        #self.ivar = ivar
+        self.N = data.shape[0]           #number of observations
+        self.D = data.shape[1]           #input dimensionality, dimension of each observed data point
+        self.Q = Q                       #latent dimensionality (note that this also includes the mean, meaning that in initialization
+                                         #                       Q-1 pca components are kept!
+        self.data = np.atleast_2d(data)  #making sure the data has the right dimension
+        self.ivar = ivar                 #variance with the same dimensiona as the data
+        
+        """ outputs of the code: NxQ amplitude (coefficients) matrix;
+                                 QxD basis (eigen vectors) matrix
+                                 NX1 flux matrix 
+        """
                
- 
-        self.A = np.zeros((self.N,self.K))
-        self.G = np.zeros((self.K,self.D))
-        self.X = np.zeros((self.D))
-        self.F = np.zeros((self.N))
+        self.A = np.zeros((self.N,self.Q))    #Creating the amplitude matrix
+        self.G = np.zeros((self.K,self.D))    #Creating the basis matrix. This matrix contains K eigen-vectors. Each eigen-vector is
+                                              # a D-dimensional object!
+        self.F = np.zeros((self.N))           #Creating an N-dimensional Flux vector. conatins flux values of N observations.
 
-        #initialize A, G, X, F:
+
+        """ initialization of FAG by means of normalizing, shifting, and singular-value decomposition"""
         self.initialize()
-        self.update_A()
-        #update A, G, X, F:
+
+        """ updating FAG matrix by F-step, A-step, and G-step least-square optimization"""
         self.update(max_iter, check_iter, min_iter, tol)
         
         
@@ -41,27 +48,30 @@ class stuff(object):
         self.dm = np.zeros((self.N,self.D))
           #shifting and normalizing
         for i in range(self.N):
+          """flux=sum"""
           self.F[i] = np.sum(self.data[i,:])
+          """shift and then normalize that shit"""
           self.dm[i,:] = shifter.shifter(self.data[i,:])/self.F[i]
+        
+        #initializing the mean. The mean will later be inserted into eigen-vectors.
+        mean = np.mean(self.dm, axis=0)
 
-        #averaging to get the mean
-        self.X = np.sum(self.dm, axis=0)/self.N
         #subtracting the mean from the shifted/normalized data
         self.dmm = self.dm - self.X
         
-        """
-        for i in range(self.N):
-          temp = self.data[i, :].reshape(self.d , self.d)[1:-1,1:-1]
-          temp = temp.reshape((self.d-2)*(self.d-2))
-          pi = shift.matrix(temp)
-          self.dmm[i] = self.data[i] - self.F[i]*np.dot(self.X,pi)""" 
-   
         #pca on the mean-subtracted shifted/normalized data
-        self.lambdas, evals = self.svd_pca(self.dmm)
-        self.G = self.lambdas[:self.K, :]
-        self.A  = self._e_step()
+        u , s , vh = la.svd(self.dmm)
         
+        #amplitudes
+        coefficients = u[:, :self.K-1] * s[None, :self.K-1]
+        ones = np.ones((self.N,1))
+        self.A = np.hstack([ones , coefficients])
+        
+        #eigen basis functions including the mean
+        self.G = np.vstack([mean , vh[:self.K-1,:]])
 
+        
+     
 
     def svd_pca(self, data):
         """
@@ -85,7 +95,7 @@ class stuff(object):
         
        
         self.PP = np.zeros((self.D,self.D))
-        self.d = int(np.sqrt(self.D))
+        self.d  = int(np.sqrt(self.D))
         self.FF = np.zeros((self.D))
         #self.dmm *= 0.
  
