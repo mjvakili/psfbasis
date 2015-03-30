c982a89c961fca23d0d32e721e68387ee4ef842c
MJ <mv1003@nyu.edu>
2015-03-30 02:57:41 -0400

    Update demo.py

--- src/demo.py
+++ src/demo.py
@@ -3,4 +3,22 @@ from stuff import stuff
 import shift
 
 data = np.loadtxt("varpsf.txt")
+Q = 6
 h = stuff(data, 2 , min_iter=1, max_iter=20, check_iter=1, tol=1.e-12)
+for j in range(maxiter):
+  
+  img = np.loadtxt("G_6(%d).txt"(%range(maxiter)[j]+1)).reshape(Q,15,15)
+  fig , axarr = plt.subplots(1,6) 
+
+
+  for i in range(Q):
+
+    axarr[i].imshow(img[i] , interpolation = "None" , origin = "lower" , cmap = cm.Greys_r)
+    axarr[i].set_xlim([-.5,14.5])
+    axarr[i].set_ylim([-.5,14.5])
+    axarr[i].set_xticks(())
+    axarr[i].set_yticks(())
+  plt.tight_layout()
+  fig.set_size_inches(28,4)
+
+  plt.savefig("G_6_(%d).png"(%range(maxiter)[j]+1)) , dpi=200)
